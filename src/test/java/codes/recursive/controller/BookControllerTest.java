b8c8b48c8bc3582e82a82bad3aac4f399996cdf7
jameskleeh <james.kleeh@gmail.com>
2020-02-03 14:38:09 -0500

    Fix usage and add test for ilike

--- src/test/java/codes/recursive/controller/BookControllerTest.java
+++ src/test/java/codes/recursive/controller/BookControllerTest.java
@@ -3,10 +3,13 @@ package codes.recursive.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.annotation.MicronautTest;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import javax.inject.Inject;
 
@@ -15,25 +18,36 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BookControllerTest {
 
     @Inject
-    EmbeddedServer embeddedServer;
+    @Client("/")
+    RxHttpClient client;
 
     @Test
     public void testIndex() throws Exception {
-        try(RxHttpClient client = embeddedServer.getApplicationContext().createBean(RxHttpClient.class, embeddedServer.getURL())) {
-            assertEquals(HttpStatus.OK, client.toBlocking().exchange("/book").status());
-        }
+        assertEquals(HttpStatus.OK, client.toBlocking().exchange("/book").status());
     }
 
     @Test
     public void testFindTitleLike() throws Exception {
-        try(RxHttpClient client = embeddedServer.getApplicationContext().createBean(RxHttpClient.class, embeddedServer.getURL())) {
-            String body = client.toBlocking().retrieve("/book/title/Semi");
-            ObjectMapper objectMapper = new ObjectMapper();
-            List result = objectMapper.readValue(body, List.class);
-            assertEquals(result.size(), 1);
-        }
+        String body = client.toBlocking().retrieve("/book/title/Semi");
+        ObjectMapper objectMapper = new ObjectMapper();
+        List result = objectMapper.readValue(body, List.class);
+        assertEquals(result.size(), 1);
+    }
+
+    @Test
+    public void testFindTitleILike() throws Exception {
+        String body = client.toBlocking().retrieve("/book/title/ilike/SEMI");
+        ObjectMapper objectMapper = new ObjectMapper();
+        List result = objectMapper.readValue(body, List.class);
+        assertEquals(result.size(), 1);
+    }
+
+    @AfterAll
+    void cleanup() {
+        client.close();
     }
 }
